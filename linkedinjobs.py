f1d3b5bb6ad19520b539d41bcdc9a094f7ce41b1
Yassir El AYADI <elayadiyassir@gmail.com>
2021-09-25 21:23:41 +0200

    second version of Linkedin Jobs code : 1 page of jobs without description

--- linkedinjobs.py
+++ linkedinjobs.py
@@ -17,65 +17,60 @@ driver = webdriver.Chrome('C:/Users/elaya/Downloads/chromedriver_win32/chromedri
 driver.implicitly_wait(10)
 driver.get('https://www.linkedin.com/')
 driver.maximize_window()
-driver.find_element_by_name('session_key').send_keys('e-mail')
-driver.find_element_by_name('session_password').send_keys('password')
+driver.find_element_by_name('session_key').send_keys('****yassir@gmail.com')
+driver.find_element_by_name('session_password').send_keys('******')
 button = driver.find_elements_by_class_name('sign-in-form__submit-button')
 button[0].click()
 driver.get('https://www.linkedin.com/jobs/')
 
 #use datetime for  ember 41&40 16h
-driver.find_element_by_id('jobs-search-box-keyword-id-ember40').send_keys('field')
-driver.find_element_by_id('jobs-search-box-location-id-ember40').send_keys('City')
+driver.find_element_by_id('jobs-search-box-keyword-id-ember40').send_keys('****')
+driver.find_element_by_id('jobs-search-box-location-id-ember40').send_keys('****')
 driver.find_element_by_xpath('//button[normalize-space()="Recherche"]').click()
 driver.find_element_by_xpath('//*[@id="ember72"]').click()
-#driver.find_element_by_xpath('//*[text()="Vous êtes dans la fenêtre de messagerie. Appuyez sur Entrée pour la réduire."]/button').click()
 
-Yeay = driver.find_element_by_css_selector('.job-card-list__entity-lockup.artdeco-entity-lockup.artdeco-entity-lockup--size-4.ember-view').text
-print(Yeay)
-
-#
-description = driver.find_element_by_xpath('//*[@id="job-details"]').text
+#description
+description = driver.find_element_by_css_selector('.jobs-search__right-rail').text
 print(description)
 
 #Recrutement actif isn't
-JRP = driver.find_element_by_xpath('//*[text()="Recrutement actif"]/ancestor::div').text
+JRP = driver.find_element_by_css_selector('ul.jobs-search-results__list.list-style-none').text 
 driver.close()
 test = JRP.split("\n")
 
 
 
-'''
 #remove the first section of web page : 
-Eff = 'Candidature simplifiée'
+Eff = ['Sponsorisé', 'Recrutement actif']
 for intro in test:
-    if Eff in intro:
-        break
-    else:
-        print(intro)
+    if any(Eff):
         test.remove(intro)
-'''
 
 #separate element : 
 chunk = [[]]
-sep = 'Sponsorisé'
+sep = ', France'
 brk = 'Le statut est en ligne'
 i = 0
 for val in test:
     if sep in val:
         i = i+1
         chunk.extend([[val]])
-        #val.replace(sep, "")
     if brk in val:
         break
     else:
         chunk[i].extend([val])
         
-print('Jobs : ', chunk, len(chunk))
+Toxcl = pd.DataFrame(chunk)
 
-for c in chunk:
-    print(len(c))
+Toxcl.to_excel("LinkedIn.xlsx", header=False, index=False)
 
-'''
 
 '''
+Toxcl.to_excel("LinkedIn.xlsx",
+             sheet_name="sheet1",
+             startrow=len(chunk),
+             startcol=0)
+'''
+
+
 
